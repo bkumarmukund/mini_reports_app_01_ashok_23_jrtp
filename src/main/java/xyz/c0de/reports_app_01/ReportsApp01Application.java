b93b48d7677377e0078b968008c0089a4c458535
Balmukund Kumar <bkumarmukund@gmail.com>
2024-08-20 21:08:16 +0530

    Initial Commit - project setup

---
+++ src/main/java/xyz/c0de/reports_app_01/ReportsApp01Application.java
@@ -0,0 +1,13 @@
+package xyz.c0de.reports_app_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsApp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/c0de/reports_app_01/ReportsApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.c0de.reports_app_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
